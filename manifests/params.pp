eeff8119ca65dfd7f22011002a353ca8fbd4b933
Nate Riffe <nriffe@backstopsolutions.com>
2014-01-31 15:39:32 -0600

    Handle admin credentials though parameterization
    In addition to credentials-as-fact, allow them to be supplied by parameter.
    Also, add a parameter to control whether the node should create the admin user,
    so that in the case whether credentials are not available from any source there
    can be a process that takes place on the stanchion node to create it (where
    create_admin should be true) and a failure elsewhere until the resulting
    credentials can be transmitted to an appropriate place.  Once established, the
    credentials should be permanent unless all of the riak data is lost, which the
    service is supposed to be hardened against.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,11 +4,13 @@ class riakcs::params {
 	$package_name        = 'riak-cs'
 	$version             = latest
     $service_name        = 'riak-cs'
-    $service_ensure      = 'running'
     $cs_ipaddress        = '127.0.0.1'
     $riak_ipaddress      = '127.0.0.1'
     $stanchion_ipaddress = '127.0.0.1'
     $vm_node_name        = 'riak-cs'
     $vm_ipaddress        = '127.0.0.1'
     $admin_email         = 'foobar@example.com'
+    $admin_key           = ''
+    $admin_secret        = ''
+    $create_admin        = false
 }
